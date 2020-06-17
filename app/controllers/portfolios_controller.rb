c1b774e308c9477eed53755283b94f3b691a14bd
Geralt of Rivia <alex@AlexH.local>
2020-06-17 08:35:53 -0500

    update status for enum

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -35,4 +35,17 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+      respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: "The record was successfully deleted." }
+      end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum"
+    body: "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum",
+    status: 0
   )
 end
 puts "10 Blog posts created!"
