db8d69bfefa6ad107f1dfe05a48f6acd7d255ecc
Geralt of Rivia <alex@AlexH.local>
2020-06-17 15:01:30 -0500

    implement image defaults for portfolio_items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,15 @@ class Portfolio < ApplicationRecord
   def self.ruby_on_rails                                             #defines scope for all Angular subtitles
     where(subtitle: 'Ruby on Rails')
   end
-end
+
+  after_initialize :set_defaults
+
+  def set_defaults                                              # ||= operator explained by the example below
+    self.main_image ||= "https://placehold.it/600x400"          # if self.main_image == nil
+    self.thumb_image ||= "https://placehold.it/350manx20"       #   self.main_image = "https://placehold.it/600x400"
+  end                                                           # end
+end                                                             #
+                                                                # self.main image = "https://..." is incorrect and will cause
+                                                                # the placehold.it image to override any photo uploaded
+
+
