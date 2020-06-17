a00690232bfe5288f6a60a12d13c7a3e8c6af811
Geralt of Rivia <alex@AlexH.local>
2020-06-17 12:48:00 -0500

    integrated database relations between topic and blog

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,4 @@
 class Portfolio < ApplicationRecord
 
-  validates_presences_of :title, :body, :thumb_image, :main_image
+  validates_presence_of :title, :body, :thumb_image, :main_image
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
+
+
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
+    topic_id: rand(1..3),
     title: "My Blog Post #{blog}",
     body: "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum",
     status: 0
@@ -25,3 +35,4 @@ puts "5 Skills created!"
   )
 end
 puts "9 Portfolio items created!"
+
