52d995e20cbc098987643f7ce05f9df9733097b9
Geralt of Rivia <alex@AlexH.local>
2020-06-18 09:29:28 -0500

    add basic authentication; fix error in skill.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'angular', to: 'portfolios#angular'
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
   resources :blogs
 
   root to: 'pages#home'
