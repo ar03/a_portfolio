37054fbf53707a5480508e9711abe741e8a9d126
Geralt of Rivia <alex@AlexH.local>
2020-06-24 09:36:02 -0500

    implement collection based partials

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -5,6 +5,6 @@ class Skill < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.badge ||=  Placeholder.image_generator(height: '250', width: '250')
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
   end
 end
