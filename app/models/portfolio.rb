158ad53fefadc5b629d0731a6a11b573aa90f2aa
Geralt of Rivia <alex@AlexH.local>
2020-07-06 18:22:40 -0500

    add position attr to portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -13,6 +13,10 @@ class Portfolio < ApplicationRecord
   def self.ruby_on_rails                                             #defines scope for all 'Ruby on Rails' subtitles
     where(subtitle: 'Ruby on Rails')
   end
+  
+  def self.by_position
+    order("position ASC")
+  end
 
   after_initialize :set_defaults
 
