afa0817fb5b80b7f67c035c035e9863245b4bf30
Alex <alexrhickey@gmail.com>
2022-01-02 13:13:22 -0600

    update placeholder | add file upload

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,12 +1,18 @@
 module PortfoliosHelper
   
+  def image_generator(height:, width:)
+    "https://placehold.it/#{height}x#{width}"
+  end
+
   def portfolio_img img, type
-    if !img.nil?
+    if img.model.main_image? || img.model.thumb_image?
       img
     elsif type == 'thumb'
       image_generator(height: '350', width: '200')
-    elsif
+    elsif type == 'main'
       image_generator(height: '600', width: '200')
     end
   end
 end
+
+
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,9 +3,8 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  include Placeholder
 
-  validates_presence_of :title, :body, :thumb_image, :main_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -18,12 +17,10 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Ruby on Rails')
   end
 
-  after_initialize :set_defaults
+  def self.by_position
+    order("position ASC")
+  end
 
-  def set_defaults                                                                    # ||= operator explained by the example below
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')      # if self.main_image == nil
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')     #   self.main_image = "https://placehold.it/600x400"
-  end                                                                                 # end
 end                                                                                   #
                                                                                       # self.main image = "https://..." is incorrect and will cause
                                                                                       # the placehold.it image to override any photo uploaded
